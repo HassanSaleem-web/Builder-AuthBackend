9c10883c663d423359f8ceb0bc38692ce00bc7d5
Hassan Saleem <smuhammadhassan39@gmail.com>
2025-11-07 17:30:34 +0500

    stripe correction

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -7,6 +7,12 @@ import {
 const router = express.Router();
 
 router.post("/create-checkout-session", createCheckoutSession);
-router.post("/webhook", handleStripeWebhook);
+
+// 👇 add raw parser here (specific to webhook)
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  handleStripeWebhook
+);
 
 export default router;
