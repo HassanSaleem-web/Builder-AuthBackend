17a429b94ec2285ea7e3a4919caa0429fc3282d1
Hassan Saleem <smuhammadhassan39@gmail.com>
2025-11-26 13:42:43 +0500

    Admin apis

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://builderassistant-3ml1.onrender.com",
+      "https://digistavadmin.onrender.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
